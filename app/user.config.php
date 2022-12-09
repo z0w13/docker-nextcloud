fc86a79c8c6abcc8f7930a3c500c9219a6b99d82
Zowie <me@zowie.cx>
2022-12-09 18:07:01 +0100

    fix: re-added database config from env vars as autoconfig gets ignored after install

--- app/user.config.php
+++ app/user.config.php
@@ -1,19 +1,50 @@
 <?php
-$CONFIG = array (
-  # Added by us
-  'installed' => $_ENV["NEXTCLOUD_INSTALLED"] == "true",
-  'overwriteprotocol' => $_ENV["OVERWRITEPROTOCOL"] ?? "",
-  'trusted_proxies' => explode(" ", $_ENV["TRUSTED_PROXIES"]),
-  'secret' => $_ENV["NEXTCLOUD_SECRET"],
-  'instanceid' => $_ENV["NEXTCLOUD_INSTANCE_ID"],
+// Based on config/autoconfig.php
+function getDbConfig() {
+  if(getenv('SQLITE_DATABASE')) {
+    return [
+      'dbtype' => 'sqlite',
+      'dbname' => getenv('SQLITE_DATABASE')
+    ];
+  }
+  if (getenv('MYSQL_DATABASE') && getenv('MYSQL_USER') && getenv('MYSQL_PASSWORD') && getenv('MYSQL_HOST')) {
+    return [
+      'dbtype' => 'mysql',
+      'dbname' => getenv('MYSQL_DATABASE'),
+      'dbuser' => getenv('MYSQL_USER'),
+      'dbpass' => getenv('MYSQL_PASSWORD'),
+      'dbhost' => getenv('MYSQL_HOST'),
+    ];
+  }
+  if (getenv('POSTGRES_DB') && getenv('POSTGRES_USER') && getenv('POSTGRES_PASSWORD') && getenv('POSTGRES_HOST')) {
+    return [
+      'dbtype' => 'pgsql',
+      'dbname' => getenv('POSTGRES_DATABASE'),
+      'dbuser' => getenv('POSTGRES_USER'),
+      'dbpass' => getenv('POSTGRES_PASSWORD'),
+      'dbhost' => getenv('POSTGRES_HOST'),
+    ];
+  }
 
-  # Technically in official image but uses weird command line shenanigans in entrypoint
-  'trusted_domains' => explode(" ", $_ENV["NEXTCLOUD_TRUSTED_DOMAINS"]),
+  return [];
+}
 
-  # Don't check for htaccess as we aren't using Apache
+// Merge the base config and the db config together
+$CONFIG = array_merge(getDbConfig(), [
+  // Custom added env vars
+  'installed' => getenv("NEXTCLOUD_INSTALLED") == "true",
+  'overwriteprotocol' => getenv("OVERWRITEPROTOCOL") ?? "",
+  'trusted_proxies' => explode(" ", getenv("TRUSTED_PROXIES")),
+  'secret' => getenv("NEXTCLOUD_SECRET"),
+  'instanceid' => getenv("NEXTCLOUD_INSTANCE_ID"),
+
+  // Technically in official image but uses weird command line shenanigans in entrypoint
+  'trusted_domains' => explode(" ", getenv("NEXTCLOUD_TRUSTED_DOMAINS")),
+
+  // Don't check for htaccess as we aren't using Apache
   'check_for_working_htaccess' => false,
-  # Don't want NextCloud messing with the config
+  // Don't want NextCloud messing with the config
   'config_is_read_only' => true,
-  # Only update through newer docker images no web updater
+  // Only update through newer docker images no web updater
   'upgrade.disable-web' => true,
-);
+]);
