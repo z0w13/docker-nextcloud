4374d7e0b76ed6667464e3f42e225875d16dbb7c
Zowie <me@zowie.cx>
2022-12-09 17:06:35 +0100

    chore: clean-up and annotate config file

--- app/user.config.php
+++ app/user.config.php
@@ -1,45 +1,19 @@
 <?php
 $CONFIG = array (
-  'trusted_domains' => explode(",", $_ENV["NEXTCLOUD_TRUSTED_DOMAINS"]),
+  # Added by us
   'installed' => $_ENV["NEXTCLOUD_INSTALLED"] == "true",
-  'overwriteprotocol' => $_ENV["NEXTCLOUD_OVERWRITE_PROTO"] ?? "",
-  'trusted_proxies' => explode(",", $_ENV["NEXTCLOUD_TRUSTED_PROXIES"]),
+  'overwriteprotocol' => $_ENV["OVERWRITEPROTOCOL"] ?? "",
+  'trusted_proxies' => explode(" ", $_ENV["TRUSTED_PROXIES"]),
   'secret' => $_ENV["NEXTCLOUD_SECRET"],
+  'instanceid' => $_ENV["NEXTCLOUD_INSTANCE_ID"],
 
-  'dbtype' => 'pgsql',
-  'dbhost' => $_ENV["DATABASE_HOST"],
-  'dbname' => $_ENV["DATABASE_NAME"],
-  'dbuser' => $_ENV["DATABASE_USER"],
-  'dbpassword' => $_ENV["DATABASE_PASSWORD"],
+  # Technically in official image but uses weird command line shenanigans in entrypoint
+  'trusted_domains' => explode(" ", $_ENV["NEXTCLOUD_TRUSTED_DOMAINS"]),
 
-  'mail_domain' => $_ENV["MAIL_DOMAIN"],
-  'mail_from_address' => $_ENV["MAIL_FROM"], // Without the domain
-  'mail_smtphost' => $_ENV["MAIL_SMTP_HOST"],
-  'mail_smtpport' => $_ENV["MAIL_SMTP_PORT"],
-  'mail_smtpsecure' => $_ENV["MAIL_SMTP_SECURE"], // SSL or TLS
-  'mail_smtpname' => $_ENV["MAIL_SMTP_USERNAME"],
-  'mail_smtppassword' => $_ENV["MAIL_SMTP_PASSWORD"],
-  'mail_smtpauth' => isset($_ENV["MAIL_SMTP_USERNAME"]),
-
+  # Don't check for htaccess as we aren't using Apache
   'check_for_working_htaccess' => false,
+  # Don't want NextCloud messing with the config
   'config_is_read_only' => true,
+  # Only update through newer docker images no web updater
   'upgrade.disable-web' => true,
-
-  'memcache.local' => '\\OC\\Memcache\\APCu',
-  'apps_paths' =>
-  array (
-    0 =>
-    array (
-      'path' => '/var/www/html/apps',
-      'url' => '/apps',
-      'writable' => false,
-    ),
-    1 =>
-    array (
-      'path' => '/var/www/html/custom_apps',
-      'url' => '/custom_apps',
-      'writable' => true,
-    ),
-  ),
-  'instanceid' => 'ocraoe7dlzc4',
 );
