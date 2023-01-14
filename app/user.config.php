97f2e091b413ab5b3a9006e8914f3f77c553e1ea
Zowie <me@zowie.cx>
2023-01-14 23:31:47 +0100

    fix: env var configuration
    * dbpass should be dbpassword
    * overwriteprotol should at least be set even if empty, otherwise getenv
      returns false

--- app/user.config.php
+++ app/user.config.php
@@ -12,16 +12,16 @@ function getDbConfig() {
       'dbtype' => 'mysql',
       'dbname' => getenv('MYSQL_DATABASE'),
       'dbuser' => getenv('MYSQL_USER'),
-      'dbpass' => getenv('MYSQL_PASSWORD'),
+      'dbpassword' => getenv('MYSQL_PASSWORD'),
       'dbhost' => getenv('MYSQL_HOST'),
     ];
   }
   if (getenv('POSTGRES_DB') && getenv('POSTGRES_USER') && getenv('POSTGRES_PASSWORD') && getenv('POSTGRES_HOST')) {
     return [
       'dbtype' => 'pgsql',
-      'dbname' => getenv('POSTGRES_DATABASE'),
+      'dbname' => getenv('POSTGRES_DB'),
       'dbuser' => getenv('POSTGRES_USER'),
-      'dbpass' => getenv('POSTGRES_PASSWORD'),
+      'dbpassword' => getenv('POSTGRES_PASSWORD'),
       'dbhost' => getenv('POSTGRES_HOST'),
     ];
   }
@@ -33,7 +33,7 @@ function getDbConfig() {
 $CONFIG = array_merge(getDbConfig(), [
   // Custom added env vars
   'installed' => getenv("NEXTCLOUD_INSTALLED") == "true",
-  'overwriteprotocol' => getenv("OVERWRITEPROTOCOL") ?? "",
+  'overwriteprotocol' => getenv("OVERWRITEPROTOCOL"),
   'trusted_proxies' => explode(" ", getenv("TRUSTED_PROXIES")),
   'secret' => getenv("NEXTCLOUD_SECRET"),
   'instanceid' => getenv("NEXTCLOUD_INSTANCE_ID"),
@@ -44,7 +44,7 @@ $CONFIG = array_merge(getDbConfig(), [
   // Don't check for htaccess as we aren't using Apache
   'check_for_working_htaccess' => false,
   // Don't want NextCloud messing with the config
-  'config_is_read_only' => true,
+  'config_is_read_only' => getenv("NEXTCLOUD_INSTALLED") == "true",
   // Only update through newer docker images no web updater
   'upgrade.disable-web' => true,
 ]);
